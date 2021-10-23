a759f6205697c4d5a97dd61181bd5539611564d8
alkmnd <asfsdg>
2021-10-24 01:41:21 +0300

    Step number 5

---
+++ nabelova_1PW4/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  nabelova_1PW4
+//
+//  Created by Наталья Белова on 24.10.2021.
+//
+
+import Foundation
+import UIKit
+struct Note {
+    let title: String
+    let description:String
+}
